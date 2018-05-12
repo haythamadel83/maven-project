a0fc77611bbe2526c1c6335ba17c802b2251efbe
root <root@localhost.localdomain>
2018-05-12 22:56:18 +0200

    fix 2 warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,15 +5,17 @@ package com.example;
  */
 public class Greeter {
 
+
+
   /**
-   * This is a constructor.
+   * This is a constructor of the class.
    */
   public Greeter() {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
